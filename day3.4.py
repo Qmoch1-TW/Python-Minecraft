18504dfc2bb7988e88a827f315e78e6f2e803cbf
Qmoch1-TW <68536853+Qmoch1-TW@users.noreply.github.com>
2020-07-22 16:47:17 +0800

    Add files via upload

---
+++ day3.4.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 11:31:26 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc= Minecraft.create()
+
+
+x,y,z =mc.player.getTilePos()
+for i in range(20):
+    mc.setBlock(x+i,y-1,z+i,41)
+    mc.setBlock(x+i+1,y-1,z+i,41)
---
+++ day3.5.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 13:46:55 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc= Minecraft.create()
+
+def plantTree(x,y,z):
+    mc.setBlocks(x,y,z,x,y+4,z,41)
+    mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,46)
+    
+
+
+x,y,z = mc.player.getTilePos()
+for i in range(100):
+    for j in range(100):
+        for k in range(100):
+            plantTree(x+i*5,y+j*5,z+k*5)
+        
+
+    
+   
+   
